1bb47c56106f6107cddd3b387eecd0d1cc8c6bdc
Astrid Hanghøj <astridhanghoej@gmail.com>
2021-08-13 10:56:21 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,15 @@
+/*Write a function that determines the length of any given word in the wordList. When button A is pressed, it should return a random word from the list and the length of that word.*/
+input.onButtonPressed(Button.A, function () {
+	
+})
+let wordList = [
+"you",
+"are",
+"getting",
+"better",
+"at",
+"coding"
+]
+basic.forever(function () {
+	
+})
